bbe15cc9fe96de9648f06b15773b7973f5ef78ce
Thomas Nordentoft <thomas@nordentoft.dk>
2026-08-17 14:21:20 +0200

    Borg dependency

--- scripts/deps_config.sh
+++ scripts/deps_config.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # scripts/deps_config.sh
+# Extra keys on top of easydeploy-lib defaults (docker, compose, openssl, curl,
+# python3, borg, borgmatic, age).
 
 easydeploy_required_deps() {
-	printf '%s\n' docker docker-compose git
+	printf '%s\n' git
 }
